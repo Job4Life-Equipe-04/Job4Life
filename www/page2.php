3a5bc38ff239c826aa4e897b8412e510931b9d1e
Florian <92437122+FlorianHO@users.noreply.github.com>
2022-03-16 11:11:47 +0100

    Add files via upload

---
+++ www/page2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="FR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Description du site web (à changer pour chaque page) - SEO">
+    <title>Page 2</title>
+    <link rel="icon" href="./assets/images/favicon.ico">
+    <link rel="stylesheet" href="./style/index.css">
+</head>
+
+<body data-barba="wrapper">
+    <header>
+        <div class="loading">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+        <div class="cont-bandes">
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+            <div class="bande"></div>
+        </div>
+        <nav>
+            <button type="button"></button>
+            <ul>
+                <li><a href="index.php">Internships</a></li>
+                <li><a href="/page2.php">Students</a></li>
+                <li><a href="/page3.html">Companies</a></li>
+                <li><a href="./contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main data-barba="container" data-barba-namespace="home">
+        <h2>Seconde page de l'app web !</h2>
+    </main>
+    <div class="cursor"></div>
+    <footer></footer>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.1/gsap.min.js"></script>
+<script src="https://unpkg.com/@barba/core"></script>
+<script src="./script/jquery-3.6.0.min.js"></script>
+<script src="./script/script.js"></script>
+
+</html>
